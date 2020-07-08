f81b9b8fac2187c364c4bcba53fe10b42f023246
haochangdi123 <haochangdi@163.com>
2020-07-08 23:35:10 +0800

    ejs pug 模版演示

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl/')
+// const ejs = require('ejs')
+const pug = require('pug') //新版的jade
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl/')
 
 app.use(async (ctx, next) => {
     ctx.type = 'text/html;charset=utf-8'
-    ctx.body = normal
+    // ctx.body = htmlTpl
+
+    // ctx.body = ejs.render(ejsTpl,{
+    //     you: 'hcd',
+    //     me: 'qwe'
+    // })
+
+    ctx.body = pug.render(pugTpl, {
+        you: 'hcd',
+        me: 'qwe'
+    })
 })
 
 
